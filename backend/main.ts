2f811c40c45d4d1c1062d647d8cfed46758d28a1
flyb1z0n <flyb1z0n@gmail.com>
2022-06-05 15:21:26 +0100

    Add TypeScript for backend

---
+++ backend/main.ts
@@ -0,0 +1,14 @@
+import { Context, Telegraf } from 'telegraf'
+
+// Create your bot and tell it about your context type
+const bot = new Telegraf<Context>(process.env.BOT_TOKEN!)
+
+// Register middleware and launch your bot as usual
+bot.use(Telegraf.log())
+bot.start((ctx) => ctx.reply('Welcome!!!112'))
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
